d16a212beb7b7958298dc49e0a02a67f8104f719
zix <zixzixzix@126.com>
2019-04-07 21:26:18 +0800

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,23 @@
-function activateGallery() {
-  alert("Hello from the gallery file!");
+function activateGallery(){
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener("click",function(){
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.alt;
+      mainImage.setAttribute("src",newImageSrc);
+      mainImage.setAttribute("alt",newImageAlt);
+
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML      = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
